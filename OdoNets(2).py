f6cdc95ef4ae437c8729575aa9717785cc37b6eb
Zhang Yifei <yifeizhang1998@qq.com>
2023-02-26 21:08:46 +0900

    Add files via upload

---
+++ OdoNets(2).py
@@ -0,0 +1,343 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import math
+device = torch.device("cuda"if torch.cuda.is_available() else "cpu")
+
+# 卷积轮速网络
+
+class CovOdoNet(nn.Module):
+
+    def __init__(self, channel1=10, channel2=64, channel3=32, fcn1=128, fcn2=64, out_dim=3):
+        super(CovOdoNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(channel1, channel2, 1)
+        self.conv2 = nn.Conv2d(channel2, channel3, 1)
+
+        self.fc1 = nn.Linear(channel3*4*1, fcn1)
+        self.fc2 = nn.Linear(fcn1, fcn2)
+        self.fc3 = nn.Linear(fcn2, out_dim)
+
+    def forward(self, x):
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+class FcnOdoNet(nn.Module):
+
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
+        super(FcnOdoNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, out_dim))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        return x
+
+
+# 全连接轮速网络
+class FcnOdoNetV2(nn.Module):
+
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, n_hidden_3, out_dim):
+        super(FcnOdoNetV2, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, n_hidden_3), nn.ReLU(True))
+        self.layer4 = nn.Sequential(nn.Linear(n_hidden_3, out_dim))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
+
+class FcnOdoNetV4(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, n_hidden_3, n_hidden_4, n_hidden_5, out_dim):
+        super(FcnOdoNetV4, self).__init__()
+        #Relu = True 代表会在原变量的基础上修改，可以节省内存或者显存，还可以省去反复申请和释放内存的时间，对输出不会有影响，只是会覆盖输入，没事儿
+        #这个相当于构建了6层，每一层激活函数是Relu，然后都是全连接层
+        #使用sigmoid作为激活函数（指数函数），运算量大，反向传播求误差梯度时，求导设计出发，计算量特别大，relu没有这个问题
+        #sigmoid函数反向传播时，很容易就出现梯度消失的情况（在sigmoid函数接近饱和区时，变换太缓慢，导数趋于0，这种情况会造成信息丢失），从而无法完成深层网络的训练
+        #Relu会使一部分神经元的输出为0，这样就造成了网络的稀疏性，并且减少了参数的相互依存关系，缓解了过拟合问题的发生
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, n_hidden_3), nn.ReLU(True))
+        self.layer4 = nn.Sequential(
+            nn.Linear(n_hidden_3, n_hidden_4), nn.ReLU(True))
+        self.layer5 = nn.Sequential(
+            nn.Linear(n_hidden_4, n_hidden_5), nn.ReLU(True))
+        self.bn = nn.BatchNorm1d(n_hidden_5)
+        self.layer6 = nn.Sequential(nn.Linear(n_hidden_5, out_dim))
+
+        self.init_weights()
+
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        return x
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Conv3d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+class FcnOdoNetV3(nn.Module):
+
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2, n_hidden_3, n_hidden_4, out_dim):
+        super(FcnOdoNetV3, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(
+            nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer3 = nn.Sequential(
+            nn.Linear(n_hidden_2, n_hidden_3), nn.ReLU(True))
+        self.layer4 = nn.Sequential(
+            nn.Linear(n_hidden_3, n_hidden_4), nn.ReLU(True))
+        self.layer5 = nn.Sequential(nn.Linear(n_hidden_4, out_dim))
+
+        self.init_weights()
+
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        return x
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Conv3d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size):
+        super(RNN, self).__init__()
+        self.rnn = nn.LSTM(
+            input_size=input_size,
+            hidden_size=64,
+            num_layers=1,
+            batch_first=True
+        )
+        self.out = nn.Sequential(
+            nn.Linear(64, 2)
+        )
+
+    def forward(self, x):
+        r_out, (h_n, h_c) = self.rnn(x, None)  
+        out = self.out(r_out)
+        return out
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Conv3d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+                    
+                    
+
+class lstm_reg(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers):
+        super(lstm_reg, self).__init__()
+
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers) # rnn
+        self.reg = nn.Linear(hidden_size, output_size) # 回归
+
+    #def init_state(self, batch_size):
+     #   return (torch.zeros(self.num_layers, batch_size, self.hidden_size),
+      #          torch.zeros(self.num_layers, batch_size, self.hidden_size))
+
+
+   
+ 
+
+
+    def forward(self, x):
+      #  hs=self.init_state(batch_size)
+        x, _ , = self.rnn(x,64) # (seq, batch, hidden)
+        s, b, h = x.shape
+        x = x.view(s*b, h) # 转换成线性层的输入格式
+        x = self.reg(x)
+        x = x.view(s, b, -1)
+        return x
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Conv3d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+
+
+#class LSTM(nn.Module):
+#    def __init__(self, input_size, hidden_layer_size=200, output_size=2):
+ #       super().__init__()
+ #       self.hidden_layer_size = hidden_layer_size
+#
+#        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+#
+ #       self.linear = nn.Linear(hidden_layer_size, output_size)
+
+ #       self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size).to(device),
+ #                           torch.zeros(1,1,self.hidden_layer_size).cuda())
+
+ #   def forward(self, input_seq):
+  #      lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+
+ #       predictions = self.linear(lstm_out.view(len(input_seq), -1))
+ #       predictions[-1] = predictions[-1].squeeze(-1)
+
+   #     return predictions[-1]
+
+
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers,output_size):
+        super().__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+           )                  
+        self.output_layer = nn.Linear(in_features=hidden_size, out_features=output_size)
+
+
+    def forward(self, input_seq,h_state):
+        lstm_out, h_state = self.lstm(input_seq )[0]
+       
+        out=[]
+        for time in range(lstm_out.size(1)):
+            every_time_out = lstm_out[:, time, :]   
+            out.append(self.output_layer(every_time_out))
+        return torch.stack(out, dim=1),h_state
+
+
+
+
+ 
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model, max_len=9144):
+        
+        super(PositionalEncoding, self).__init__()      
+        
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        #pe.requires_grad = False
+        self.register_buffer('pe', pe)
+ 
+    def forward(self, x):
+      #  print("PositionalEncoding",x.size())
+        
+        
+        return x + self.pe[:x.size(0), :]
+          
+ 
+class TransAm(nn.Module):
+    def __init__(self,feature_size=600,num_layers=1,dropout=0.1):
+        super(TransAm, self).__init__()
+        self.model_type = 'Transformer'
+        
+        self.src_mask = None
+        self.pos_encoder = PositionalEncoding(feature_size)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=6, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
+        self.decoder = nn.Linear(feature_size*20, 6)
+        self.init_weights()
+        
+        self.feature_size = feature_size
+        self.num_layers   = num_layers
+        self.dropout      = dropout
+        
+    def feature(self):
+        return{"feature_size":self.feature_size,"num_layers":self.num_layers,"dropout":self.dropout}
+        
+ 
+    def init_weights(self):
+        initrange = 0.1    
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+ 
+    def forward(self,src):
+        #print("0",src.shape)
+        if self.src_mask is None or self.src_mask.size(0) != len(src):
+            device = src.device
+            mask = self._generate_square_subsequent_mask(len(src)).to(device)
+            self.src_mask = mask
+        #print("1",src.shape)
+        src = self.pos_encoder(src)
+        # print("2",src.shape)
+        # print("src:", self.src_mask.shape)
+        output = self.transformer_encoder(src,self.src_mask)
+        # print("output:{}".format(self.src_mask))
+        output = output.view(output.shape[0], -1)
+        #print("3",output.shape)
+        output = self.decoder(output)
+        #print('4',output.shape)
+
+        return output
+ 
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
---
+++ vr_train_localpose_for_cuda(4).py
@@ -0,0 +1,171 @@
+import time
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.utils as utils
+import numpy as np
+import os
+import sys
+import math
+from losses.vr_PoseLoss import LocalPoseLoss
+from losses.vr_PoseLoss import PoseLoss2D
+from dataset.vr_posedataset import PoseDataSet
+from OdoNets import FcnOdoNetV4
+from OdoNets import TransAm
+import matplotlib.pyplot as plt
+import warnings
+import torch.cuda.amp as amp
+
+# 实例化 GradScaler
+scaler = amp.GradScaler()
+
+warnings.filterwarnings('ignore')
+np.seterr(invalid='ignore')
+
+device = torch.device("cuda"if torch.cuda.is_available() else "cpu")
+
+def train_conv_odo_imu_net(train_dataset, checkpoint_path):
+    # 训练的参数
+    batch_size = 20
+    learning_rate = 0.005
+    epochs = 100
+    odo_frames = 40 #？？？我目前理解，这个就是batch Size，代表每次选40帧原始数据进行训练
+    # 读入数据
+    dataset_list = []
+    for dir in os.listdir(train_dataset):
+        dataset = os.path.join(train_dataset, dir) + "/0000/ODO"
+        dataset_list.append(dataset)
+    dataset = PoseDataSet(dataset_list, odo_frames)
+    #用十个子进程加载数据， 每个epoch重新打乱数据，pin:内存的Tensor转义到GPU的显存会更快一些
+    train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=14, shuffle=True, pin_memory=True)
+    print("load dataset finished")
+    #定义网络 6层 7参 输入10*40维度 输出2维度
+    # odonet = FcnOdoNetV4(15 * odo_frames, 1024, 521, 256, 128, 64, 6)
+    odonet =TransAm(    feature_size=15 * odo_frames  ,num_layers=1,dropout=0.01  )
+    odonet = odonet.to(device)
+    # odonet = nn.DataParallel(odonet)
+    # model.train()的作用是启用 Batch Normalization 和 Dropout。如果模型中有BN层(Batch Normalization）和Dropout，需要在训练时添加model.train()。model.train()是保证BN层能够用到每一批数据的均值和方差。对于Dropout，model.train()是随机取一部分网络连接来训练更新参数。
+    #model.eval()的作用是不启用 Batch Normalization 和 Dropout。如果模型中有BN层(Batch Normalization）和Dropout，在测试时添加model.eval()。model.eval()是保证BN层能够用全部训练数据的均值和方差，即测试过程中要保证BN层的均值和方差不变。对于Dropout，model.eval()是利用到了所有网络连接，即不进行随机舍弃神经元。训练完train样本后，生成的模型model要用来测试样本。在model(test)之前，需要加上model.eval()，否则的话，有输入数据，即使不训练，它也会改变权值。这是model中含有BN层和Dropout所带来的的性质。
+
+    #定义loss5
+    #SGD 是最普通的优化器, 也可以说没有加速效果, 而 Momentum 是 SGD 的改良版, 它加入了动量原则. 后面的 RMSprop 又是 Momentum 的升级版. 而 Adam 又是 RMSprop 的升级版. 不过从这个结果中我们看到, Adam 的效果似乎比 RMSprop 要差一点. 所以说并不是越先进的优化器, 结果越佳. 我们在自己的试验中可以尝试不同的优化器, 找到那个最适合你数据/网络的优化器
+    optimizer = optim.SGD(odonet.parameters(), lr=learning_rate) #SGD比ADAM效果好
+    # optimizer = torch.optim.AdamW(odonet.parameters(), lr = learning_rate)
+    # pose_loss = LocalPoseLoss()
+    pose_loss = PoseLoss2D(0.004)
+    #训练网络
+
+    print('Start training:   .......................................................')
+    torch.autograd.set_detect_anomaly(True)
+
+    loss_list = []
+    for epoch in range(epochs):
+        odonet.train() #设置训练模式  使用PyTorch进行训练和测试时一定注意要把实例化的model指定train/eval。model.eval()时，框架会自动把BN和Dropout固定住，不会取平均，而是用训练好的值
+        epoch_loss = []
+        rot_error = []
+        trans_error = []
+
+        # if(epoch == 130):
+        #     optimizer = optim.SGD(odonet.parameters(), lr=(learning_rate * 0.1))
+
+            # torch.save({
+            # 'epoch': epoch,
+            # 'model_state_dict': odonet.state_dict(),
+            # 'optimizer_state_dict': optimizer.state_dict(),
+            # 'loss': loss}, checkpoint_path[0:-6]+"_index60.pth")
+        # if(epoch == 300):
+        #     optimizer = optim.SGD(odonet.parameters(), lr=(learning_rate * 0.01))
+        #     torch.save({
+        #     'epoch': epoch,
+        #     'model_state_dict': odonet.state_dict(),
+        #     'optimizer_state_dict': optimizer.state_dict(),
+        #     'loss': loss}, checkpoint_path[0:-6]+"_index300.pth")
+
+        for batch_idx, (raw_odo, gt_pose) in enumerate(train_loader):
+            raw_odo = raw_odo.to(device).to(torch.float32)
+            #将每个输入中的每个批次都拉成一个维度，也就是40帧输入不变
+            raw_odo = raw_odo.view(raw_odo.size(0), -1)
+            gt_pose = gt_pose.to(device).to(torch.float32)
+            if raw_odo.shape[0] < batch_size:
+                break
+            #梯度清零
+            optimizer.zero_grad()
+
+            # 进入自动混合精度上下文
+            with amp.autocast():
+                #前向传播，将原始数据变成1*2的预测
+                pred_pose_euler = odonet.forward(raw_odo)
+                # print("pred_pose_euler:{}".format(pred_pose_euler))
+                loss = pose_loss(gt_pose, pred_pose_euler)
+                # print("loss-typep:{}".format(loss))
+                # print(loss)
+
+            # 缩放损失
+            scaler.scale(loss).backward(retain_graph = True)
+            # loss.backward()
+            # 使用autograd.grad()计算梯度
+            # grads = torch.autograd.grad(loss, odonet.parameters(), retain_graph=True)
+            max_norm = 1.0  # 设置最大的 L2 范数为 1
+            grad_norm = utils.clip_grad_norm_(odonet.parameters(), max_norm)
+
+
+            scaler.step(optimizer)
+            # optimizer.step()
+
+            scaler.update()
+
+            epoch_loss.append(loss.data.item())
+            rot_error.append(pose_loss.rot_error.data.item())
+            trans_error.append(pose_loss.trans_error.data.item())
+
+        print('epoch: {}, loss: {:.10}, rot_err: {:.10}, trans_err: {:.10}'\
+        .format(epoch, np.mean(np.array(epoch_loss)),\
+                       np.mean(np.array(rot_error)), \
+                       np.mean(np.array(trans_error))))
+        if epoch % 10 == 0:
+            torch.save({
+            'epoch': epoch,
+            'model_state_dict': odonet.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss}, checkpoint_path + str(epoch).zfill(4)+".pth")
+
+        loss_list.append(loss)
+
+
+    print(type(loss_list))
+    plt.figure()
+#plt.plot(range(0, len(P_train_loss)), P_train_loss, 'r', label='P_Training loss')
+    plt.plot(range(1, len(loss_list)+1),
+         loss_list, 'r', label='Training loss')
+
+#plt.plot(range(1, len(P_test_loss)+1), P_test_loss,
+    #     'bo', label='P_Validation loss')
+
+    #plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss Value')
+    plt.legend()
+    plt.show()#
+
+
+    #plt.plot(range(1, len(R_test_loss)+1), R_test_loss,
+        #     'g', label='R_Validation loss')
+
+
+if __name__ == '__main__':
+
+    SEED = 0
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+
+    #训练集目录
+   # train_dataset = "/data1/MAF_DATASET/421_POSE/"
+    train_dataset = 'C:/zyf_transformer/transformer/data_vr_move/'
+
+    #网络参数保存位置
+    # checkpoint_path = "/home/everglow/momenta_workspace/odo_net/checkpoints/local_pose/421_local_pose_40frame"
+    checkpoint_path = 'C:/zyf_transformer/transformer/data_vr_move/'
+
+    #训练网络
+    train_conv_odo_imu_net(train_dataset, checkpoint_path)
